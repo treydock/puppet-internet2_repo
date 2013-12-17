6e9251a1371c9c8540ca4712223ee13d6a207a3e
Trey Dockendorf <treydock@gmail.com>
2013-12-17 12:41:54 -0600

    Fix libdir not loading in Puppet tests for version 3.0.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+min_version = Gem::Version.create('3.0.0')
+max_version = Gem::Version.create('3.1.0')
+puppet_version = Gem::Version.create(`puppet --version`.strip)
+
+if puppet_version > min_version and puppet_version < max_version
+  $:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'stdlib', 'lib')
+  $:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'gpg_key', 'lib')
+end
+
 shared_context :defaults do
   let(:node) { 'foo.example.tld' }
 
